4973bf9a237cf2f0287e0c35386a32e51fafc3f7
Veranika <veranika.bazhanok@gmail.com>
2024-12-26 16:21:54 +0100

    feat: implement solution find

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -55,9 +55,18 @@ class BinarySearchTree {
 		}
 	}
 
-	find(/* data */) {
-		throw new NotImplementedError('Not implemented');
-		// remove line with error and write your code here
+	find(data) {
+		return findWithin(this.rootNode, data);
+
+		function findWithin(node, data) {
+			if (!node) {
+				return null;
+			}
+			if (node.data === data) {
+				return node;
+			}
+			return data < node.data ? findWithin(node.left, data) : findWithin(node.right, data);
+		}
 	}
 
 	remove(data) {
